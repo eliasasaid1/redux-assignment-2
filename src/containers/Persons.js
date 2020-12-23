5b72bfe598dc5aaaf57ade9e9800d566e48534d9
elias.asaid <eliasisseid@gmail.com>
2020-12-22 19:36:47 -0500

    first commit

---
+++ src/containers/Persons.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+import * as actionTypes from '../store/actions';
+class Persons extends Component {
+
+
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.onAddPerson} />
+                {this.props.persons.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.OnDeletePerson(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const generateNewPerson = () => {
+    return {
+        id: Math.random(), // not really unique but good enough here!
+        name: 'Max',
+        age: Math.floor( Math.random() * 40 )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        persons : state.persons
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddPerson : () => dispatch({type: actionTypes.ADD_PERSON,person: generateNewPerson()}),
+        OnDeletePerson : (id) => dispatch({type : actionTypes.DELETE_PERSON,id : id }),
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Persons);
---
+++ src/store/reducer.js
@@ -0,0 +1,27 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    persons : []
+};
+
+
+
+const reducer = (state = initialState, action) => {
+
+    switch(action.type){
+        case actionTypes.ADD_PERSON:  
+        return {
+            ...state,
+            persons: state.persons.concat(action.person)
+        }
+        case actionTypes.DELETE_PERSON : 
+        const updatedArray = state.persons.filter(result => result.id !== action.id);
+        return {
+            ...state,
+            persons: updatedArray
+        }
+        default : return state;
+    }
+};
+
+export default reducer;
